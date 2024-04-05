af68f6832b5a7666efef5397623d969fa39f5f68
Chris Pring <chrisjlpring@outlook.com>
2024-04-05 10:49:43 +0100

    Added homepage.js and moved logic to generate html there

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-import MyCat from './cat.jpg';
+import component from './homepage';
 
-function component() {
-    const content = document.getElementById("content");
-
-    const heading = document.createElement("h1");
-    heading.textContent = "This is a heading";
-    content.appendChild(heading);
-
-    const catImage = new Image();
-    catImage.src = MyCat;
-    content.appendChild(catImage);
-
-    const paragraph = document.createElement("p");
-    paragraph.textContent = `Wow this restaurant is fancy
-    more text about how great it is
-    you can get food here
-    mmmmmmmmmmmmmmmmmmmmmmmmmmmm`;
-    content.appendChild(paragraph);
-}
 component();
